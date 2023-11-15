6565bfebb4a7c7c7df962c623ec8b0c2a8dd6bb1
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-11-15 20:15:14 +0100

    updat

--- src/Loop.jsx
+++ src/Loop.jsx
@@ -1,3 +1,5 @@
+import googles from './assets/desktop/image-interactive.jpg'
+
 function Loops() {
     return (
         <div className="container">
@@ -17,6 +19,13 @@ function Loops() {
                     <p>immersive experiences that deliver</p>
                 </div>
             </header>
+            <div className="vr">
+                <img src={googles} alt="vr googles"/>
+                <section>
+                    <h1>THE LEADER IN INTERACTIVE VR</h1>
+                    <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+                </section>
+            </div>
         </div>
     );
 }
