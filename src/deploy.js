649c4282fcd588faa72295bea9c10504f3819010
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-11-15 14:54:53 +0100

    updte

---
+++ src/deploy.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXjIXsYD96CxRQ15JFCSBNOMR2AwGPGjw",
+  authDomain: "adeloopstudio.firebaseapp.com",
+  projectId: "adeloopstudio",
+  storageBucket: "adeloopstudio.appspot.com",
+  messagingSenderId: "1042746389631",
+  appId: "1:1042746389631:web:626f39a00ad8915948bb4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
