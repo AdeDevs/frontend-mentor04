9dda2a152103ed50e5b1aa29ba099dc6f5724b9d
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-11-16 23:49:10 +0100

    updt

--- src/Loop.jsx
+++ src/Loop.jsx
@@ -1,10 +1,8 @@
-import googles from './assets/desktop/image-interactive.jpg'
-import Inter from './Inter.jsx';
-
+// import googles from './assets/desktop/image-interactive.jpg'
 function Loops() {
     return (
         <div className="container">
-            <header>
+            <header className="menu">
                 <nav>
                     <h1>loopstudios</h1>
 
@@ -20,7 +18,31 @@ function Loops() {
                     <p>immersive experiences that deliver</p>
                 </div>
             </header>
-            <Inter />
+            <section className="vr">
+                <div></div>
+                <div>
+                    <article>
+                    <h1>THE LEADER IN INTERACTIVE VR</h1>
+                    <p>Founed in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+                    </article>
+                </div>
+            </section>
+            <section className="creations">
+                <header>
+                    <h1>OUR CREATIONS</h1>
+                    <button>SEE ALL</button>
+                </header>
+                <section>
+                    <div><p>DEEP EARTH</p></div>
+                    <div><p>NIGHT ARCADE</p></div>
+                    <div><p>SOCCER TEAM VR</p></div>
+                    <div><p>THE GRID</p></div>
+                    <div><p>FROM UP ABOVE VR</p></div>
+                    <div><p>POCKET BOREALIS</p></div>
+                    <div><p>THE CURIOSITY</p></div>
+                    <div><p>MAKE IT FISHEYE</p></div>
+                </section>
+            </section>
         </div>
     );
 }
